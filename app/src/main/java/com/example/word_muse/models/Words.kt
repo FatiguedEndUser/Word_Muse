1b6ba314997c6191ed880321bed6db8482efcdc1
FatiguedEndUser <tylor.magadan@protonmail.com>
2024-10-29 14:26:12 -0500

    Fixed formatting

--- app/src/main/java/com/example/word_muse/models/Words.kt
+++ app/src/main/java/com/example/word_muse/models/Words.kt
@@ -2,21 +2,24 @@
 
 package com.example.word_muse.models
 
-data class Words(var word: String,
-                 var phonetic: String,
-                 var phonetics: List<Phonetics>,
-                 var origin: String,
-                 var meaning: String,
-                 )
-
-data class Definitions(var definition: String,
-                       var example: String,
-                       var synonyms: List<String>,
-                       var antonyms: List<String>
+data class Words(
+    var word: String,
+    var phonetic: String,
+    var phonetics: List<Phonetics>,
+    var origin: String,
+    var meaning: String
 )
 
-data class Meaning(var partOfSpeech: String,
-                   var definitions: List<Definitions>
+data class Definitions(
+    var definition: String,
+    var example: String,
+    var synonyms: List<String>,
+    var antonyms: List<String>
+)
+
+data class Meaning(
+    var partOfSpeech: String,
+    var definitions: List<Definitions>
 )
 
 data class Phonetics(
