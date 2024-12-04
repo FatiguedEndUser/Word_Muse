1ec26ad1d79f26527db7608b15b77e56252b32aa
FatiguedEndUser <tylor.magadan@protonmail.com>
2024-12-04 15:41:47 -0600

    Reworking API services to actually get it functioning

--- app/src/main/java/com/example/word_muse/ApiService.kt
+++ app/src/main/java/com/example/word_muse/ApiService.kt
@@ -1,28 +1,11 @@
 package com.example.word_muse
 
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
-
-
-private val URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-private val retrofit = Retrofit
-    .Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
-    .baseUrl(URL)
-    .build()
-
 
 interface SearchApiService{
     //Async
     @GET("word")
-    suspend fun search(query: String): String
-}
-
-public object DictionaryAPI{
-    val retrofitService: SearchApiService by lazy {
-        retrofit.create(SearchApiService::class.java)
-    }
+    suspend fun search(query: String): List<String>
 }
 
 
---
+++ app/src/main/java/com/example/word_muse/RetrofitClient.kt
@@ -0,0 +1,25 @@
+package com.example.word_muse
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import okhttp3.logging.HttpLoggingInterceptor
+
+object RetrofitClient {
+    private val URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+    private val logging = HttpLogginInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val okHttpClient = OkHttpClient.Builder
+        .addInterceptor(logging)
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+
+    val wordApiService: SearchApiService by lazy{
+
+    }
+}
